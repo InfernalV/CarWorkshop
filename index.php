61be535d653eac0bca44d8e2ca4c55d510875635
Mage0Foxy <91876215+Mage0Foxy@users.noreply.github.com>
2022-03-10 14:49:29 +0100

    versie 0.7

--- index.php
+++ index.php
@@ -4,28 +4,31 @@ include("includes/Functions.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="./css/workshop.css" type="text/css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">-->
     <title>Document</title>
 </head>
 <body>
-<form action="/">
-    <div class="mb-3 mt-3">
+<form method="post" action="index.php">
+    <div class="Username">
         <label for="username" class="form-label">username:</label>
         <input type="username" class="form-control" id="username" placeholder="Enter username" name="username">
     </div>
-    <div class="mb-3">
+    <div class="Password">
         <label for="pwd" class="form-label">Password:</label>
         <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="password">
     </div>
-    <div class="form-check mb-3">
+    <div class="Remember">
         <label class="form-check-label">
             <input class="form-check-input" type="checkbox" name="remember"> Remember me
         </label>
     </div>
+    <div class="Submit">
         <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
 </form>
     <a href="nav.php"<input type="submit"/>Nav
 </body>
--- insert_info.php
+++ insert_info.php
@@ -5,15 +5,16 @@ add_time($conn);
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="./css/workshop.css" type="text/css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<!--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">-->
     <title>Document</title>
 </head>
 <body>
 <form class="" action="insert_info.php" method="post">
-    <div>
+    <div class="day">
         <select class="form-select" id="sel1" name="sellist1">
         <option>-----select day-----</option>
         <?php days($conn) ?>
@@ -21,13 +22,19 @@ add_time($conn);
     </div>
 </form>
 <form class="" action="insert_info.php" method="post">
-    <div>
+    <div class="time">
         <select class="form-select" id="sel1" name="sellist1">
             <option>-----select time-----</option>
             <?php times($conn) ?>
         </select>
     </div>
 </form>
+<form class="" action="insert_info.php" method="post">
+    <div class="SubmitInf">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+</form>
+
 
 <a href="nav.php"<input type="submit"/>Nav
 <a href="week.php"<input type="submit"/>Week
