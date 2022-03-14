35e3c078300ff6ba49b49172504640eae750b311
Mage0Foxy <91876215+Mage0Foxy@users.noreply.github.com>
2022-03-14 10:43:17 +0100

    Versie 0.9

--- forum.php
+++ forum.php
@@ -8,11 +8,46 @@ include("includes/Functions.php")
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">-->
+    <!--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">-->
     <title>Document</title>
+    <div class="Logo"><img src="Images/J&B_Logo.png" alt="Null"> </div>
 </head>
 <body>
-<a href="nav.php"<input type="submit"/>Nav
-<a href="task.php"<input type="submit"/>Task
+<form method="post" action="forum.php">
+
+    <div class="Project">
+        <label for="username" class="form-label">username:</label>
+        <input type="username" class="form-control" id="username" placeholder="Enter username" name="username">
+    </div>
+    <div class="Client">
+        <label for="pwd" class="form-label">Password:</label>
+        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="password">
+    </div>
+    <div class="Workload">
+        <label class="form-check-label">
+            <input class="form-check-input" type="checkbox" name="remember"> Remember me
+        </label>
+    </div>
+    <div class="Description">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+
+</form>
+<div class="row">
+    <div class="col-3">
+        <div class="mButton1">
+            <a href="nav.php"<input type="submit"/>Nav
+        </div>
+        <div class="mButton2">
+            <a href="task.php"<input type="submit"/>Task
+        </div>
+        <div class="mButton3">
+            <a href="employee.php"<input type="submit"/>Employee
+        </div>
+        <div class="mButton4">
+            <a href="week.php"<input type="submit"/>Week
+        </div>
+    </div>
+</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -10,6 +10,7 @@ include("includes/Functions.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">-->
     <title>Document</title>
+    <div class="Logo"><img src="Images/J&B_Logo.png" alt="Null"> </div>
 </head>
 <body>
 <form method="post" action="index.php">
